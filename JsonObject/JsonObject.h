c5d4ea420a5d76741da9993f7d22809dbe9a7177
Ntobeko Sikithi <ntobeko.sikithi@standardbank.co.za>
2019-10-21 09:30:29 +0200

    Initial project setup

---
+++ JsonObject/JsonObject.h
@@ -0,0 +1,19 @@
+//
+//  JsonObject.h
+//  JsonObject
+//
+//  Created by Ntobeko Sikithi on 2019/10/21.
+//  Copyright © 2019 Ntobeko Sikithi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JsonObject.
+FOUNDATION_EXPORT double JsonObjectVersionNumber;
+
+//! Project version string for JsonObject.
+FOUNDATION_EXPORT const unsigned char JsonObjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JsonObject/PublicHeader.h>
+
+
---
+++ JsonObjectExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JsonObjectExample
+//
+//  Created by Ntobeko Sikithi on 2019/10/21.
+//  Copyright © 2019 Ntobeko Sikithi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
